268b58954d2e70647a949b19447843e9326887e9
Eric Gustavo Rocha Durigon <dev.ericgusta@gmail.com>
2026-05-08 11:13:56 -0300

    Estrutura organizada com pasta de interface e arquivo CSV

---
+++ InicialStructure.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+typedef enum { Sim = 1, Nao } Repetida;
+
+typedef struct{
+    char codigo[6];
+    char titulo[35];
+    char secao[25];
+    char tipo[10];
+    int quantidade;
+    Repetida repetida;
+} Figurinha;
+
+int main(){
+
+    return 0;
+}
